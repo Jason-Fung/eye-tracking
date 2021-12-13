87d1c016f8e305417d3c5373d6a953f58e639cda
Jason_F <35511431+Jason-Fung@users.noreply.github.com>
2021-12-13 02:15:04 -0800

    Implemented kalman filter
    need to fix online streaming

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtGui import QPixmap, QImage
 from mainwindow import *
 from processing_functions import *
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from typing import *
+import pandas as pd
+
 
 class capture(Ui_MainWindow):
     def __init__(self):
@@ -22,8 +29,49 @@ class capture(Ui_MainWindow):
         Ui_MainWindow.setupUi(self,MainWindow)
         self.CameraButton.clicked.connect(self.onClicked)
         self.StopViewButton.clicked.connect(self.offClicked)
+        self.Save_Data_Button.clicked.connect(self.save_data_click)
+
+
         self.ThresholdSlider.valueChanged.connect(self.handleSliderValueChange)
         self.ThresholdValue.valueChanged.connect(self.handleBoxValueChange)
+        
+        self.leftThreshSlider.valueChanged.connect(self.lefthandleSliderValueChange)
+        self.leftThresholdValue.valueChanged.connect(self.lefthandleBoxValueChange)
+
+        self.rightThreshSlider.valueChanged.connect(self.righthandleSliderValueChange)
+        self.rightThresholdValue.valueChanged.connect(self.righthandleBoxValueChange)
+
+        # set up left eye data streaming window
+        self.LE_X_series_Box = QtWidgets.QVBoxLayout()
+        self.LeftEye_X_Series.setLayout(self.LE_X_series_Box)
+        self.LE_Y_series_Box = QtWidgets.QVBoxLayout()
+        self.LeftEye_Y_Series.setLayout(self.LE_Y_series_Box)
+
+        # set up right eye data streaming window
+        self.RE_X_series_Box = QtWidgets.QVBoxLayout()
+        self.RightEye_X_Series.setLayout(self.RE_X_series_Box)
+        self.RE_Y_series_Box = QtWidgets.QVBoxLayout()
+        self.RightEye_Y_Series.setLayout(self.RE_Y_series_Box)
+
+
+        # Instantiate figure canvas and place into widget
+        # x coordinate time series
+        self.myFig_LE_X = MyFigureCanvas(x_len = 200, y_range = [0,40], interval = 1)
+        self.LE_X_series_Box.addWidget(self.myFig_LE_X)
+        # y coordinate time series
+        # myFig_LE_Y = MyFigureCanvas(x_len = 200, y_range = [0,40], interval = 1)
+        self.myFig_LE_Y = MyFigureCanvas(x_len = 200, y_range = [0,40], interval = 1)
+        self.LE_Y_series_Box.addWidget(self.myFig_LE_Y)
+
+        # Instantiate figure canvas and place into widget
+        # x coordinate time series
+        self.myFig_RE_X = MyFigureCanvas(x_len = 200, y_range = [0,40], interval = 1)
+        self.RE_X_series_Box.addWidget(self.myFig_RE_X)
+        # y coordinate time series
+        # myFig_LE_Y = MyFigureCanvas(x_len = 200, y_range = [0,40], interval = 1)
+        self.myFig_RE_Y = MyFigureCanvas(x_len = 200, y_range = [0,40], interval = 1)
+        self.RE_Y_series_Box.addWidget(self.myFig_RE_Y)
+
 
     def handleSliderValueChange(self, value):
         # function is to connect with box value 
@@ -32,6 +80,22 @@ class capture(Ui_MainWindow):
     def handleBoxValueChange(self, value):
         # function is to connect with slider 
         self.ThresholdSlider.setValue(value)
+
+    def lefthandleSliderValueChange(self, value):
+        # function is to connect with box value 
+        self.leftThresholdValue.setValue(value)
+
+    def lefthandleBoxValueChange(self, value):
+        # function is to connect with slider 
+        self.leftThreshSlider.setValue(value)
+
+    def righthandleSliderValueChange(self, value):
+        # function is to connect with box value 
+        self.rightThresholdValue.setValue(value)
+
+    def righthandleBoxValueChange(self, value):
+        # function is to connect with slider 
+        self.rightThreshSlider.setValue(value)
 
     def calibrateAdaThresh(self, value):
         # work on adaptive thresholding
@@ -43,10 +107,46 @@ class capture(Ui_MainWindow):
         # function onClicked() starts video streaming
 
         detector, face_cascade, eye_cascade = init_settings()
-
         _translate = QtCore.QCoreApplication.translate
         self.CameraLabel.setTitle(_translate("MainWindow","Active"))
         print("Webcam has connected")
+
+        # Original Kalman Filter Pre-process matrices
+        # assume that the eye follows a linear model x(t) = x_o + vdt
+
+        trans_matrix = np.array([[1, 1/60, 0, 0],
+                                [0, 1, 0, 0],
+                                [0, 0, 1, 1/60],
+                                [0, 0, 0, 1]])
+
+        init_state_mean = np.array([20,
+                                    0,
+                                    20,
+                                    0])
+
+        observ_matrix = [[1, 0, 0, 0],
+                         [0, 0, 1, 0]]
+        
+        # Covariance Matrices
+        observ_cov = np.diag([0.3**2,0.3**2]) # observation covariance
+        state_cov = np.diag([0, 1, 0, 1]) # initial state covariance
+        trans_cov = np.diag([0.01, 0.05, 0.01, 0.05])
+
+        kf = KalmanFilter(transition_matrices= trans_matrix,
+						observation_matrices = observ_matrix,
+						initial_state_mean = init_state_mean,
+						observation_covariance = observ_cov,
+						initial_state_covariance = state_cov,
+						transition_covariance = trans_cov,
+                        em_vars = ['transition_covariance', 'observation_covariance'])
+
+        # data 
+        left_eye_data = []
+        right_eye_data = []
+
+        # set data limit for efficient data storage
+        data_lim = 500
+
         # start capturing video
         cap = cv2.VideoCapture(0)
         self.cap = cap
@@ -55,47 +155,128 @@ class capture(Ui_MainWindow):
             ret, frame = cap.read()
             frame = cv2.flip(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB),1) # flip horizontally
 
-                # eye_h, eye_w, eye_c = eye.shape # h = height, w = width, c = channel
-                # left_eye, right_eye = eyes
-                # bytesPerLine_eye = 3 * width
-
-
             if ret == True:
                 face = detect_faces(frame, face_cascade)
+                # print(np.shape(face))
                 if face is not None: # check for face
 
                     left_eye, right_eye, coords = detect_eyes(face, eye_cascade)
-
+                
                     # Use threshold for both eyes. IN THE FUTURE IMPLEMENT INDIVIDUAL THRESHOLD
                     # FOR EACH EYE
                     threshold = self.ThresholdSlider.value()
                     self.threshold = threshold
 
-                    # show rectangular window around eye  
+                    left_threshold = self.leftThreshSlider.value()
+                    self.left_threshold = left_threshold
 
-                    # cv2.rectangle(face,(le_x, le_y),(le_x+le_w,le_y+le_h),(0,225,255),2) # left eye
-                    # cv2.rectangle(face,(le_x, le_y),(le_x+le_w,le_y+le_h),(0,225,255),2) # right eye
+                    right_threshold = self.rightThreshSlider.value()
+                    self.right_threshold = right_threshold
 
                     if left_eye is not None: # check for a left eye
                         if self.leftCheckBox.isChecked():
-                            _, keypoints = blob_process(left_eye, self.threshold, detector)
-                            left_eye = draw_blobs(left_eye, keypoints)
+                            _, l_keypoints = blob_process(left_eye, self.left_threshold, detector)
+                            if l_keypoints is not ():
+                                left_eye_data.append([l_keypoints[0].pt[0],l_keypoints[0].pt[1]])
 
+                                if len(left_eye_data) == data_lim+1:
+                                    # update the list of positions if the length of the array reaches
+                                    # the data limit for efficient storage.
+                                    # Pop the first value of the list after appending the new coordinates.
+                                    left_eye_data.pop(0)
+                                
+                                self.left_eye_data = left_eye_data
+                                if self.Track_X_LE.isChecked():
+                                    self.myFig_LE_X._update_canvas_(data = l_keypoints[0].pt[0])
+                                    self.myFig_LE_Y._update_canvas_(data = l_keypoints[0].pt[1])
+                            
+                            else:
+                                left_eye_data.append(np.array([0,0]))
+                                if len(left_eye_data) == data_lim+1:
+                                    # update the list of positions if the length of the array reaches
+                                    # the data limit for efficient storage.
+                                    # Pop the first value of the list after appending the new coordinates.
+                                    left_eye_data.pop(0)
+                            
+                            # apply the kalman filter 
+                            if self.KF_CheckBox.isChecked():
+                                # create new keypoints
+                                # kf.em(left_eye_data, n_iter = 2)
+                                l_next_mean, _ = kf.smooth(left_eye_data)
+                                # print(next_mean)
+                                self.left_filtered_data = l_next_mean
+                                l_tmp = l_next_mean[-1]
+                                l_new_keypoints = []
+                                l_new_keypoints.append(cv2.KeyPoint(l_tmp[0],l_tmp[2],15))
+                                left_eye = draw_blobs(left_eye, l_new_keypoints, color = [0,255,0])
+                                # print(l_new_keypoints[0].pt[0] - l_keypoints[0].pt[0])
+
+                            left_eye = draw_blobs(left_eye, l_keypoints)
+                    
                         # ensure that image is compatible to display left eye in right window
                         left_eye = np.require(left_eye, np.uint8, 'C')
                         # left_eye = draw_blobs(left_eye, keypoints)
                         self.display_eye_image(left_eye, 'left')
+                        
+                    else: # does not find eye
+                        self.myFig_LE_X._update_canvas_(data = 0)
+                        self.myFig_LE_Y._update_canvas_(data = 0)
 
-                    if right_eye is not None: # check for a right eye
-                        if self.rightCheckBox.isChecked():
-                            _, keypoints = blob_process(right_eye, self.threshold, detector)
-                            right_eye = draw_blobs(right_eye, keypoints)
+                    if right_eye is not None: # check for a left eye
+                        if self.leftCheckBox.isChecked():
+                            _, r_keypoints = blob_process(right_eye, self.right_threshold, detector)
+                            if r_keypoints is not ():
+                                right_eye_data.append([r_keypoints[0].pt[0],r_keypoints[0].pt[1]])
 
-                        # ensure that image is compatible to display right eye in right window
-                        right_eye = np.require(right_eye, np.uint8, 'C')
-                        # right_eye = draw_blobs(right_eye, keypoints)
-                        self.display_eye_image(right_eye, 'right')
+                                if len(right_eye_data) == data_lim+1:
+                                    # update the list of positions if the length of the array reaches
+                                    # the data limit for efficient storage.
+                                    # Pop the first value of the list after appending the new coordinates.
+                                    right_eye_data.pop(0)
+                                
+                                self.right_eye_data = right_eye_data
+                                if self.Track_X_RE.isChecked():
+                                    self.myFig_RE_X._update_canvas_(data = r_keypoints[0].pt[0])
+                                    self.myFig_RE_Y._update_canvas_(data = r_keypoints[0].pt[1])
                             
+                            else:
+                                right_eye_data.append(np.array([0,0]))
+                                if len(right_eye_data) == data_lim+1:
+                                    # update the list of positions if the length of the array reaches
+                                    # the data limit for efficient storage.
+                                    # Pop the first value of the list after appending the new coordinates.
+                                    right_eye_data.pop(0)
+                            
+                            # apply the kalman filter 
+                            if self.KF_CheckBox.isChecked():
+                                # create new keypoints
+                                # kf.em(left_eye_data, n_iter = 2)
+                                r_next_mean, _ = kf.smooth(right_eye_data)
+                                # print(next_mean)
+                                self.right_filtered_data = r_next_mean
+                                r_tmp = r_next_mean[-1]
+                                r_new_keypoints = []
+                                r_new_keypoints.append(cv2.KeyPoint(r_tmp[0],r_tmp[2],15))
+                                right_eye = draw_blobs(right_eye, r_new_keypoints, color = [0,255,0])
+                                # print(l_new_keypoints[0].pt[0] - l_keypoints[0].pt[0])
+
+                            right_eye = draw_blobs(right_eye, r_keypoints)
+                    
+                        # ensure that image is compatible to display left eye in right window
+                        right_eye = np.require(right_eye, np.uint8, 'C')
+                        # left_eye = draw_blobs(left_eye, keypoints)
+                        self.display_eye_image(right_eye, 'right')
+                        
+                    else: # does not find eye
+                        self.myFig_RE_X._update_canvas_(data = 0)
+                        self.myFig_RE_Y._update_canvas_(data = 0)
+                
+                else: # does not find face
+                    self.myFig_LE_X._update_canvas_(data = 0)
+                    self.myFig_LE_Y._update_canvas_(data = 0)
+                    self.myFig_RE_X._update_canvas_(data = 0)
+                    self.myFig_RE_Y._update_canvas_(data = 0)
+
                 # Put capturing frame into QLabel in Qt 
                 height, width, channel = frame.shape
                 bytesPerLine = 3 * width
@@ -109,6 +290,14 @@ class capture(Ui_MainWindow):
         _translate = QtCore.QCoreApplication.translate
         self.CameraLabel.setTitle(_translate("MainWindow","Not Active"))
         self.cap.release()
+    
+    def save_data_click(self):
+        new_list = [self.left_eye_data,self.right_eye_data, 
+                    self.left_filtered_data,self.right_filtered_data]
+        df = pd.DataFrame(new_list)
+        df.to_csv('test_data.csv')
+        # function save_data_click() saves a segment of time series data 
+
 
     def display_eye_image(self, img, window):
         # function eye_image() displays captured eyes onto QT Gui interface
@@ -127,6 +316,42 @@ class capture(Ui_MainWindow):
         if window == 'right':
             self.RightEyeWindow.setPixmap(QPixmap.fromImage(output_img))
             self.RightEyeWindow.setScaledContents(True)
+
+
+class MyFigureCanvas(FigureCanvas):
+    # FigureCanvas used to draw live plot 
+    def __init__(self, x_len:int, y_range:list, interval:int) -> None:
+        super().__init__(mpl.figure.Figure())
+        # Range Settings
+        self._x_len = x_len
+        self._y_range_ = y_range
+
+        # store two lists _x_ and y
+        self._x_ = list(range(0,x_len))
+        self._y_ = [0]*x_len
+
+        # store a figure ax
+        self._ax_ = self.figure.subplots()
+        self._ax_.set_ylim(ymin = self._y_range_[0], ymax = self._y_range_[1])
+        self._line_, = self._ax_.plot(self._x_,self._y_)
+        self.draw()
+
+        # # set timer
+        # self._timer_ = self.new_timer(interval, [(self._update_canvas_, (), {})])
+        # self._timer_.start()
+        return
+    
+    def _update_canvas_(self, data):
+        # function gets called regularly by timer
+
+        self._y_.append(data)
+        self._y_ = self._y_[-self._x_len:] # truncate list y
+        self._line_.set_ydata(self._y_)
+        self._ax_.draw_artist(self._ax_.patch)
+        self._ax_.draw_artist(self._line_)
+        self.update()
+        self.flush_events()
+        return
 
 if __name__ == "__main__":
     import sys
--- processing_functions.py
+++ processing_functions.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from pykalman import KalmanFilter
 
 def init_settings():
 	# function init_settings() sets up the necessary parameters for performing computer
@@ -8,10 +8,12 @@ def init_settings():
 	# Returns detector, face_cascade = face classifier, eye_cascade = eye classifier
 
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+	eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+
 	detector_params = cv2.SimpleBlobDetector_Params()
-	detector_params.filterByArea = True
+	# detector_params.filterByArea = True
 	detector_params.maxArea = 1500
+
 	detector = cv2.SimpleBlobDetector_create(detector_params)
 	return detector, face_cascade, eye_cascade
 
@@ -37,19 +39,19 @@ def detect_eyes(img,classifier):
 	# function takes cut out face (img) and gray face (img_gray) and chosen type 
 	# of cascade filter (classifier) to select eyes. Function returns the images of
 	# the left and right eyes.
-
 	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to gray scale for eye detection
-	coords = classifier.detectMultiScale(img_gray,1.05,5) # detect eyes
+	coords = classifier.detectMultiScale(img_gray,1.3,5) # detect eyes
 	height = np.size(img,0) # get face frame height
 	width = np.size(img,1) # get face frame width
 
 	left_eye = None
 	right_eye = None
+
 	for (x,y,w,h) in coords:
 		if y + h > height/2: # only accept objects that are above the first half of face
 			pass
-		eyecenter = x + w/2 # get the approx eye center by cutting face in half
-		if eyecenter < width*0.5:
+		face_center = x + w/2 # get the approx eye center by cutting face in half
+		if face_center < width*0.5:
 			left_eye = img[y:y+h,x:x+w]
 			# left_eye_coords = [x, x+w, y, y+w]
 		else:
@@ -72,6 +74,8 @@ def blob_process(img, threshold, detector):
 	gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert BGR/RGB space to gray space
 	_, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
 
+	# img = cv2.adaptiveThreshold(gray_frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 7, 4)
+
 	img = cv2.erode(img, None, iterations = 2) #1 process
 	img = cv2.dilate(img, None, iterations = 4) #2 process
 	img = cv2.medianBlur(img, 5) #3 process
@@ -85,6 +89,28 @@ def blob_process(img, threshold, detector):
 def nothing(x):
 	pass
 
-def draw_blobs(img, keypoints):
-	return cv2.drawKeypoints(img, keypoints, img, (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+def draw_blobs(img, keypoints, color = [255,0,0]):
+	return cv2.drawKeypoints(img, keypoints, img, color, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
+# introduce Kalman Filter to assist eye detection
+
+def performKalman(data, trans_mat, state_mean, observ_mat, 
+				  observ_cov, state_cov, trans_cov):
+	
+	# data: list [x,y] where x and y are the coordinates of the pupil
+	# trans_mat: square matrix of size (2n,2n) where n is the number of states
+	# state_mean: np array of size (1,2n). Initial mean state 
+	# observ_cov: np diag of size (n,n).
+	# state_cov: np diag of size (2n,2n).
+	# trans_cov: np diag of size (2n,2n). 
+
+	kf = KalmanFilter(transition_matrices= trans_mat,
+						observation_matrices = observ_mat,
+						initial_state_mean = state_mean,
+						observation_covariance = observ_cov,
+						initial_state_covariance = state_cov,
+						transition_covariance = trans_cov)
+	
+	next_mean, next_cov = kf.smooth(data)
+
+	return next_mean, next_cov
